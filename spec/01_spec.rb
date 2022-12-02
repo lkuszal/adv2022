20f9a4153f77a0362df4dbf538aeac3e714f6377
lkuszal <lkuszal1@gmail.com>
2022-12-02 20:02:47 +0100

    test fixup

--- spec/01_spec.rb
+++ spec/01_spec.rb
@@ -1,9 +1,9 @@
-require '01/day_01.rb'
+require 'day_01/calories_counter'
 
-describe "CaloriesCounter" do
+describe CaloriesCounter do
   context "find_biggest_value" do
     it "calculates elf with the most calories" do
-      test_input = "test_inputs/test_input_01.txt"
+      test_input = "./spec/test_inputs/test_input_01.txt"
       calories_counter = described_class.new(test_input)
       expect((calories_counter).find_biggest_value).to eq 24000
     end
