78b22178cc57fdcabd1f0151f1fdc50d9c9ecaa3
lkuszal <lkuszal1@gmail.com>
2022-12-03 20:15:40 +0100

    create day3 test

---
+++ spec/03_spec.rb
@@ -0,0 +1,12 @@
+require 'day_03/rucksack_manager'
+
+describe RucksackManager do
+  test_input = "./spec/test_inputs/test_input_03.txt"
+  rucksack_manager = described_class.new(test_input)
+
+  context "priority_of_duplicated_items" do
+    it "get and add priorities of all duplicated items in all backpacks" do
+      expect((rucksack_manager).priority_of_duplicated_items).to eq 157
+    end
+  end
+end
